a4d7c35ef63ed7370f5b4d91767a1517e75e3781
Chady Chaito <chady.chaito@estudante.ufscar.br>
2022-03-20 11:21:14 -0300

    ✅ (Login): Ensure will not load spinner and error on start

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Styles from './form-status-styles.scss'
 import Spinner from '../spinner/spinner'
+import Context from '@/presentation/contexts/form/formContext'
 
-const FormStatus: React.FC = () => (
-  <div className={Styles.errorWrap}>
-    <Spinner className={Styles.spinner} />
-    <span className={Styles.error}>Erro</span>
-  </div>
-)
+const FormStatus: React.FC = () => {
+  const { isLoading, errorMessage } = useContext(Context)
+  return (
+    <div className={Styles.errorWrap} data-testid="error-wrap">
+      {isLoading && <Spinner className={Styles.spinner} />}
+      {errorMessage && <span className={Styles.error}>{errorMessage}</span>}
+    </div>
+  )
+}
 
 export default FormStatus
